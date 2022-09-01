c13ce71f7cf02b4a7e7923da7ecae51b28f9cfbb
rrrcoba <roger-decroly@hotmail.com>
2022-09-01 12:16:30 +0200

    Exercise 13 finished

---
+++ 13/js/code.js
@@ -0,0 +1,11 @@
+document.getElementById("text").onclick = function () {
+    alert("Has clicado en el primer párrafo.");
+}
+
+document.getElementById("table1").onclick = function () {
+    alert("Has clicado un párrafo de la primera tabla.");
+}
+
+document.getElementById("table2").onclick = function () {
+    alert("Has clicado un párrafo de la segunda tabla.");
+}
